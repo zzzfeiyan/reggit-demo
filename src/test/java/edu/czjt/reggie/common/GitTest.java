74645016cbeba7fdcf0c530e954c5e2eb67384d6
18789829806 <hbxylsh@163.com>
2023-06-15 14:09:26 +0800

    后端项目

---
+++ src/test/java/edu/czjt/reggie/common/GitTest.java
@@ -0,0 +1,10 @@
+package edu.czjt.reggie.common;
+
+/**
+ * Created by jinkun.tian on 2023/5/24
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hehe");
+    }
+}
